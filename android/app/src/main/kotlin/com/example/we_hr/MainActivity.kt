808fbd71640115093b1fa1838e7cff1bde3dd705
mano299 <mano299ahmed1@gmail.com>
2025-09-02 23:13:04 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/we_hr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.we_hr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WeHR());
+}
+
+class WeHR extends StatelessWidget {
+  const WeHR({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WeHR',
+    );
+  }
+}
