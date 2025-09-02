3dcae01b5f6dce4be38b000c2c0b8f15eef8f7b8
mano299 <mano299ahmed1@gmail.com>
2025-09-02 23:22:27 +0300

    create dashboard view to continue

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:we_hr/widgets/adaptive_layout.dart';
+import 'package:we_hr/widgets/dashboard_layout.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveLayout(
+      mobileLayout: (context) => SizedBox(),
+      tabletLayout: (context) => SizedBox(),
+      desktopLayout: (context) => DashboardLayout(),
+    );
+  }
+}
